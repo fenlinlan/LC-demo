0bfe4e51afa38a605bac3b0569cecf924bf5f134
wzlai <y31j4jp6@gmail.com>
2022-08-15 01:56:46 +0800

    update storyall

--- src/app/home/story/gods/gods.page.ts
+++ src/app/home/story/gods/gods.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GODDATA } from 'src/app/models/gods.model';
+import { goddata } from 'src/app/services/mock-gods';
+
 
 @Component({
   selector: 'app-gods',
@@ -8,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
 export class GodsPage implements OnInit {
 
   constructor() { }
-
+  godone : GODDATA[] = goddata;
+  godid :GODDATA = goddata[0]; 
   ngOnInit() {
   }
+  
+  checkdata():void{
+   var i = 0;
+   if (i > goddata.length){
+    i = 0
+   }else{
+    i = i++
+   }
+   this.godid = goddata[i];
+  }
+
+
 
 }
--- src/app/home/story/history/history.page.ts
+++ src/app/home/story/history/history.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
